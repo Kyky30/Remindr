a3cf097f2dc15b286eff0c2f49867c24d0a332ca
Dorian Rochette <dorian.rochette@etu.uca.fr>
2023-12-21 17:21:24 +0100

    Add new CSS classes and update HTML structure in style.css and style.sass files Update table structure and add CSS class in dashboard.handlebars file

--- public/js/filter.js
+++ public/js/filter.js
@@ -1,116 +1,119 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const viewAllRow = document.getElementById('viewAllRow');
-    const groupRows = document.querySelectorAll('.groupRow');
-    const rappelsTable = document.getElementById('rappelsTable');
+document.addEventListener("DOMContentLoaded", function () {
+    // Attend que le DOM soit chargé
 
- // Fonction pour trier les rappels par date d'échéance
-    function sortRappelsByDate(rappelGroup) {
-        const rappels = Array.from(rappelGroup.children).sort((a, b) => {
-            const dateAString = a.dataset.date;
-            const dateBString = b.dataset.date;
+    // Sélectionne tous les éléments de la classe 'rappelGroup'
+    var rappelRows = document.querySelectorAll('.rappelGroup tr');
 
-            if (dateAString && dateBString) {
-                const dateA = new Date(dateAString);
-                const dateB = new Date(dateBString);
+    // Parcours chaque ligne de rappel
+    rappelRows.forEach(function (row) {
+        // Obtient la date d'échéance de la ligne actuelle
+        var dateEcheance = new Date(row.getAttribute('data-date'));
 
-                return dateA - dateB;
-            } else {
-                console.error('Attribut data-date manquant:', a, b);
-                return 0;
-            }
-        });
+        // Obtient la date actuelle
+        var currentDate = new Date();
 
-        rappelGroup.innerHTML = ''; // Vide la liste actuelle
-
-        rappels.forEach((rappel) => {
-            rappelGroup.appendChild(rappel); // Ajoute les rappels triés
-        });
-    }
-    
-  // Fonction pour mettre à jour le filtre
-  function updateFilter(groupName) {
-    // Logique pour mettre à jour le filtre selon le groupe sélectionné
-    // Vous pouvez implémenter votre propre logique ici
-
-    // Exemple: Afficher ou masquer les lignes de rappel en fonction du groupe
-    const allRappelGroups = document.querySelectorAll('.rappelGroup');
-    allRappelGroups.forEach(function (rappelGroup) {
-        if (groupName === 'View All' || rappelGroup.dataset.groupName === groupName) {
-            rappelGroup.style.display = 'table-row-group';
-            sortRappelsByDate(rappelGroup);
-            highlightOverdueRappels(rappelGroup);
-        } else {
-            rappelGroup.style.display = 'none';
+        // Compare les dates
+        if (dateEcheance < currentDate) {
+            // Si la date d'échéance est dépassée, applique un arrière-plan rouge
+            // applique la classe expired
+            row.classList.add('expired');
         }
-    });
-
-    // Ajouter la classe selected-filter à l'élément actuellement sélectionné
-    // Supprimer la classe des autres éléments
-    groupRows.forEach(function (row) {
-        if (row.dataset.groupName === groupName) {
-            row.classList.add('selected-filter');
-        } else {
-            row.classList.remove('selected-filter');
-            viewAllRow.classList.remove('selected-filter');
-        }
-    });
-
-        // Ajouter la classe selected-filter à l'élément "View All" s'il est sélectionné
-        // Retirer la classe si un autre filtre est sélectionné
-        if (groupName === 'View All') {
-            viewAllRow.classList.add('selected-filter');
-        } else {
-            viewAllRow.classList.remove('selected-filter');
-        }
-    }
-
-    // Fonction pour mettre en surbrillance les rappels dont la date est dépassée
-    function highlightOverdueRappels(rappelGroup) {
-        const today = new Date();
-
-        Array.from(rappelGroup.children).forEach((rappel) => {
-            const dateString = rappel.dataset.date;
-            console.log(dateString);
-
-            if (dateString) {
-                const dateecheance = new Date(dateString);
-
-                if (!isNaN(dateecheance)) {
-                    if (dateecheance < today) {
-                        rappel.style.backgroundColor = 'red';
-                    } else {
-                        rappel.style.backgroundColor = ''; // Réinitialise la couleur de fond
-                    }
-                } else {
-                    console.error('Date invalide:', dateString);
-                }
-            } else {
-                console.error('Attribut data-date manquant:', rappel);
-            }
-        });
-    }
-
-    // Initialise la page avec "View All" en bleu et sans texte de filtre
-    updateFilter('View All');
-
-    // Ajoutez un gestionnaire d'événements pour chaque ligne de groupe
-    groupRows.forEach(function (row) {
-        row.addEventListener('click', function () {
-            const groupName = row.dataset.groupName;
-            console.log(row.dataset.groupName);
-            updateFilter(groupName);
-        });
-    });
-
-    // Ajoutez un gestionnaire d'événements pour la ligne "View All"
-    viewAllRow.addEventListener('click', function () {
-        // Logique pour afficher tous les éléments (réinitialiser le filtre)
-        const allRappelGroups = document.querySelectorAll('.rappelGroup');
-        allRappelGroups.forEach(function (rappelGroup) {
-            rappelGroup.style.display = 'table-row-group';
-            sortRappelsByDate(rappelGroup);
-            highlightOverdueRappels(rappelGroup);
-        });
-        updateFilter('View All');
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Attend que le DOM soit chargé
+
+    // Sélectionne le tbody contenant les rappels
+    var rappelsTableBody = document.getElementById('rappelsTable').getElementsByTagName('tbody')[0];
+
+    // Obtient toutes les lignes de rappel
+    var rappelRows = rappelsTableBody.getElementsByTagName('tr');
+
+    // Convertit les lignes de rappel en un tableau pour le tri
+    var rappelArray = Array.from(rappelRows);
+
+    // Trie le tableau par date d'échéance (du plus urgent au moins urgent)
+    rappelArray.sort(function (a, b) {
+        var dateA = new Date(a.getAttribute('data-date'));
+        var dateB = new Date(b.getAttribute('data-date'));
+
+        return dateA - dateB;
+    });
+
+    // Ajoute les lignes triées au tbody
+    rappelArray.forEach(function (row) {
+        rappelsTableBody.appendChild(row);
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Select all elements with the class 'groupRow'
+    const groupRows = document.querySelectorAll('.groupRow');
+    //remove the class selected-filter from all rows
+    // Add a click event listener to each row
+    groupRows.forEach(row => {
+        row.addEventListener('click', function() {
+            removeSelectedFilter()
+            // Log the group name
+            //Ajoute la classe selected-filter à la ligne cliquée
+            this.classList.add('selected-filter');
+            console.log(this.dataset.groupName);
+            hideRemindersNotInGroup(this.dataset.groupName);
+        });
+    });
+});
+
+
+// Wait for the DOM to load
+document.addEventListener('DOMContentLoaded', function() {
+    // Select the element with the id 'viewAllRow'
+    const viewAllRow = document.getElementById('viewAllRow');
+
+    // Add a click event listener to the element
+    viewAllRow.addEventListener('click', function() {
+        showAllReminders(); //Affiche tous les rappels
+        document.getElementById('viewAllRow').classList.add('selected-filter');
+    });
+});
+
+
+function hideRemindersNotInGroup(groupName) {
+    // Select all reminder elements
+    // enleve la classe selected-filter de viewAllRow
+    document.getElementById('viewAllRow').classList.remove('selected-filter');
+    var rappelRows = document.querySelectorAll('.rappelGroup tr');
+
+    // Loop through each reminder
+    rappelRows.forEach(function(row) {
+        var RawGroupName = row.getAttribute('data-group-name');
+        // If the reminder does not belong to the group, hide it
+        if (RawGroupName !== groupName) {
+            row.style.display = 'none';
+        } else {
+            // Otherwise, make sure it's visible
+            row.style.display = '';
+        }
+    });
+}
+function showAllReminders() {
+    // Select all reminder elements
+    removeSelectedFilter();
+    var rappelRows = document.querySelectorAll('.rappelGroup tr');
+    // Loop through each reminder
+    rappelRows.forEach(function(row) {
+        // Make sure the reminder is visible
+        row.style.display = '';
+    });
+}
+
+function removeSelectedFilter() {
+    // Select all elements with the class 'selected-filter'
+    const selectedElements = document.querySelectorAll('.selected-filter');
+
+    // Loop through each element
+    selectedElements.forEach(element => {
+        // Remove the class 'selected-filter'
+        element.classList.remove('selected-filter');
+    });
+}
